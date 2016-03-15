1d54e30f87bae2a12b50568f3d4ecb0085d99fa1
ericvnwk <eric.vnwk@gmail.com>
2016-03-14 17:47:30 -0700

    Huge content update
    Updated content, fixed landing page, added transition from landing
    page, fixed jQuery functions, streamlined classes, and a few minor
    details

--- js/main.js
+++ js/main.js
@@ -1,20 +1,13 @@
 $('.superhero a').click(
   function(){
-    $('#batman').removeClass('slide')
-
+    $( $(this).attr('href')).removeClass('slide')
+    $('#superheroes').fadeOut(250)
   }
 )
 
-$('.btn-box a').click(
+$('.btn-box a, .character-info a').click(
   function(){
     $('.text-box').fadeOut(500)
     $( $(this).attr('href') ).fadeIn(750);
-  }
-)
-
-$('.btn-box a').click(
-  function(){
-    $('.text-box').addClass('pop')
-    $( $(this).attr('href') ).removeClass('pop')
   }
 )
